9ada72a3072e7afca821472dca0076e98d6d810d
Echoha <50481720+Echoha@users.noreply.github.com>
2020-03-08 13:16:06 +0800

    修改layers，添加适量tag

--- style-transfer/src/layers.py
+++ style-transfer/src/layers.py
@@ -1,20 +1,19 @@
 import tensorflow as tf
 from functools import reduce
 
+
 def conv_layer(net, num_filters, filter_size, strides, style_control=None, relu=True, name='conv'):
     with tf.variable_scope(name):
         b,w,h,c = net.get_shape().as_list()
         weights_shape = [filter_size, filter_size, c, num_filters]
         weights_init = tf.get_variable(name, shape=weights_shape, initializer=tf.truncated_normal_initializer(stddev=.01))
         strides_shape = [1, strides, strides, 1]
-
-        p = (filter_size - 1) // 2
+        p = int((filter_size - 1) / 2)
         if strides == 1:
-            net = tf.pad(net, [[0, 0], [int(p), int(p)], [int(p), int(p)], [0, 0]], "REFLECT")
+            net = tf.pad(net, [[0, 0], [p, p], [p, p], [0, 0]], "REFLECT")
             net = tf.nn.conv2d(net, weights_init, strides_shape, padding="VALID")
         else:
             net = tf.nn.conv2d(net, weights_init, strides_shape, padding="SAME")
-
         net = conditional_instance_norm(net, style_control=style_control)
         if relu:
             net = tf.nn.relu(net)
@@ -27,11 +26,8 @@ def conv_tranpose_layer(net, num_filters, filter_size, strides, style_control=No
         b, w, h, c = net.get_shape().as_list()
         weights_shape = [filter_size, filter_size, num_filters, c]
         weights_init = tf.get_variable(name, shape=weights_shape, initializer=tf.truncated_normal_initializer(stddev=.01))
-
         batch_size, rows, cols, in_channels = [i.value for i in net.get_shape()]
         new_rows, new_cols = int(rows * strides), int(cols * strides)
-        # new_shape = #tf.pack([tf.shape(net)[0], new_rows, new_cols, num_filters])
-
         new_shape = [batch_size, new_rows, new_cols, num_filters]
         tf_shape = tf.stack(new_shape)
         strides_shape = [1,strides,strides,1]
@@ -47,11 +43,12 @@ def conv_tranpose_layer(net, num_filters, filter_size, strides, style_control=No
     return tf.nn.relu(net)
 
 
-def residual_block(net, filter_size=3, style_control=None, name='res'):
+
+def residual_block(net, num_filters=128, filter_size=3, style_control=None, name='res'):
     with tf.variable_scope(name+'_a'):
-        tmp = conv_layer(net, 128, filter_size, 1, style_control=style_control)
+        tmp = conv_layer(net, num_filters, filter_size, 1, style_control=style_control)
     with tf.variable_scope(name+'_b'):
-        output = net + conv_layer(tmp, 128, filter_size, 1, style_control=style_control, relu=False)
+        output = net + conv_layer(tmp, num_filters, filter_size, 1, style_control=style_control, relu=False)
     return output
 
 
@@ -63,20 +60,25 @@ def conditional_instance_norm(net, style_control=None, name='cond_in'):
         var_shape = [channels]
         shift = []
         scale = []
-
-        for i in range(len(style_control)):
+        # 1
+        for i in range(style_control.shape[0]):
             with tf.variable_scope('{0}'.format(i) + '_style'):
                 shift.append(tf.get_variable('shift', shape=var_shape, initializer=tf.constant_initializer(0.)))
                 scale.append(tf.get_variable('scale', shape=var_shape, initializer=tf.constant_initializer(1.)))
+        shift = tf.convert_to_tensor(shift)
+        scale = tf.convert_to_tensor(scale)
         epsilon = 1e-3
         normalized = (net-mu)/(sigma_sq + epsilon)**(.5)
-
-        idx = [i for i, x in enumerate(style_control) if not x == 0]
-
-        style_scale = reduce(tf.add, [scale[i]*style_control[i] for i in idx]) / sum(style_control)
-        style_shift = reduce(tf.add, [shift[i]*style_control[i] for i in idx]) / sum(style_control)
+        idx = tf.where(tf.not_equal(style_control, tf.constant(0, dtype=tf.float32)))
+        style_select = tf.gather(style_control, idx)
+        scale_select = tf.gather_nd(scale, idx)
+        shift_select = tf.gather_nd(shift, idx)
+        style_scale = tf.reduce_sum(scale_select * style_select, axis=0)
+        style_shift = tf.reduce_sum(shift_select * style_select, axis=0)
+        style_sum = tf.reduce_sum(style_control)
+        style_scale = style_scale / style_sum
+        style_shift = style_shift / style_sum
         output = style_scale * normalized + style_shift
-
     return output
 
 
